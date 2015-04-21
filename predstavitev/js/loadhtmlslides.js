e8652cc3c4631a22f3e5236a1e898f07b6dd91cc
Marko Grešak <marko@gresak.io>
2015-04-21 23:04:34 +0200

    show title with js
    Dirty hack to fix positioning.

--- predstavitev/js/loadhtmlslides.js
+++ predstavitev/js/loadhtmlslides.js
@@ -43,4 +43,39 @@
     };
 
     querySlidingHtml();
+
+    var titleEl = document.createElement('div');
+    titleEl.innerHTML = '<div class="background">' +
+        '<div class="frame-wrapper"><iframe src="https://vine.co/v/eztZAqdEgAp/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
+        '<div class="frame-wrapper"><iframe src="https://vine.co/v/OY6W0tvId07/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
+        '<div class="frame-wrapper"><iframe src="https://vine.co/v/OLh9Tb17XZ0/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
+        '</div><div class="content-main-title"><h2>Podatkovno rudarjenje</h2><h4>Mreze v socialnem omrezju Vine</h4><div><small>Marko Grešak</small></div>' +
+        '<small>Mentorja: doc. dr. Tomaž Curk, Matrin Stražar</small></div>';
+
+    var showTitle = function (rmClass) {
+        if (rmClass) {
+            titleEl.className = '';
+        }
+        titleEl.style.display = 'block';
+        document.querySelector('.backgrounds').appendChild(titleEl);
+    };
+
+    var hideTitle = function () {
+        titleEl.style.display = 'none';
+    };
+
+    window.onload = function () {
+        showTitle();
+
+        Reveal.addEventListener('slidechanged', function() {
+            if (Reveal.isFirstSlide()) {
+                setTimeout(function () {
+                    showTitle(true);
+                }, 250);
+            }
+            else {
+                hideTitle();
+            }
+        });
+    };
 })();
