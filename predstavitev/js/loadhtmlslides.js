6a71f5679e0bdf5b077b49d6e0fbc64217227856
Marko Grešak <marko@gresak.io>
2015-04-21 23:12:55 +0200

    add change second video to looping one

--- predstavitev/js/loadhtmlslides.js
+++ predstavitev/js/loadhtmlslides.js
@@ -47,7 +47,7 @@
     var titleEl = document.createElement('div');
     titleEl.innerHTML = '<div class="background">' +
         '<div class="frame-wrapper"><iframe src="https://vine.co/v/eztZAqdEgAp/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
-        '<div class="frame-wrapper"><iframe src="https://vine.co/v/OY6W0tvId07/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
+        '<div class="frame-wrapper"><iframe src="https://vine.co/v/hETDUMLddV6/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
         '<div class="frame-wrapper"><iframe src="https://vine.co/v/OLh9Tb17XZ0/embed/postcard" width="300" height="480" frameborder="0"></iframe></div>' +
         '</div><div class="content-main-title"><h2>Podatkovno rudarjenje</h2><h4>Mreze v socialnem omrezju Vine</h4><div><small>Marko Grešak</small></div>' +
         '<small>Mentorja: doc. dr. Tomaž Curk, Matrin Stražar</small></div>';
