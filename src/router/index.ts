093e0d69a1253473553078b9821ca08cccd2db28
Marko Grešak <marko@gresak.io>
2015-05-24 07:44:18 +0200

    properly implement router's api

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,73 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 
-let app = express();
+class Router {
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+  private address: string;
 
-var port = 9631;
+  /**
+   * Start a new express server on given port.
+   *
+   * @param   {number} port Port to listen on.
+   */
+  constructor(port: number) {
+    this.address = null;
+    let app = express();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use('/router', this.setupExpressRouter());
+    app.listen(port);
+    console.log('Router listening on ' + port);
+  }
 
-var router = express.Router();
+  /**
+   * Setup express router. Adds methods:
+   * 	- get: get currently stored address
+   * 	- put: add (overwrite) address, returns address
+   * 	- delete: delete stored address, returns address (undefined)
+   *
+   * @returns {express.Router} Configured express router.
+   */
+  private setupExpressRouter(): express.Router {
+    let router = express.Router();
+    router.get('/', (req, res) => {res.json(this.getAddress())});
+    router.put('/:address', (req, res) => {res.json(this.putAddress(req.body.address || req.params.address))});
+    router.delete('/', (req, res) => {res.json(this.delAddress())});
+    return router;
+  }
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+  /**
+   * Get curretly stored address.
+   *
+   * @returns {Object} IP address in form { address: xxx.xxx.xxx.xxx }.
+   */
+  private getAddress(): Object {
+    return { address: this.address };
+  }
 
-app.use('/api', router);
+  /**
+   * Add (overwrite) stored address.
+   *
+   * @param   {string} address New IP address.
+   *
+   * @returns {Object}         Return value from getAddress (new address).
+   */
+  private putAddress(address: string) {
+    this.address = address || null;
+    return this.getAddress();
+  }
 
-app.listen(port);
-console.log('Router listening on ' + port);
+  /**
+   * Remove stored address.
+   *
+   * @returns {Object} Return value from getAddress -> {address: undefined}.
+   */
+  private delAddress() {
+    this.address = null;
+    return this.getAddress();
+  }
+
+}
+
+// Start a new router on port 9631.
+new Router(9631);
