9b7ad13f377ef2eff6255e6d1bcac617a0e24d67
Marko Grešak <marko@gresak.io>
2015-05-24 06:59:54 +0200

    add orchestrate module

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 "use strict";
 import VineApi from "./api/VineApi";
+import {VineHelper} from "./api/ApiHelpers";
+let Orchestrate = require("orchestrate");
 
 let username: string = process.env.VINE_USERNAME;
 let password: string = process.env.VINE_PASSWORD;
+let orchestrateKey: string = process.env.ORCHESTRATE_KEY;
+let db = Orchestrate(orchestrateKey);
 
 function startVine() {
   let vine = new VineApi();
