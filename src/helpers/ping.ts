fed4d335584728069bafa2b5f3baf0cd527fb531
Marko Grešak <marko@gresak.io>
2015-05-24 09:45:58 +0200

    add httpResponse to check call

--- src/helpers/ping.ts
+++ src/helpers/ping.ts
@@ -2,16 +2,16 @@ import {Promise} from "es6-promise";
 import {get} from "request";
 
 /**
- * Ping a server each `interval`ms until `checker` returns true.
+ * Ping a server each `interval`ms until `check` returns true.
  *
- * @param   {string}                  address  Address to ping.
- * @param   {string}                  endpoint Address endpoint to ping (starting / is not needed).
- * @param   {number}                  interval Interval in ms to ping the server.
- * @param   {(string) => boolean}     checker  Function which returns true when result is as expected.
+ * @param   {string}                    address  Address to ping.
+ * @param   {string}                    endpoint Address endpoint to ping (starting / is not needed).
+ * @param   {number}                    interval Interval in ms to ping the server.
+ * @param   {(string, any?) => boolean} check    Function which returns true when result is as expected.
  *
- * @returns {Promise<any>}                     Promise which is resolved when checker returns true.
+ * @returns {Promise<any>}                     Promise which is resolved when `check` returns true.
  */
-export function ping(address: string, endpoint: string, interval: number, checker: (res: string) => boolean, thisArg: any = this): Promise<any> {
+export function ping(address: string, endpoint: string, interval: number, check: (body: string, res?: any) => boolean, thisArg: any = this): Promise<any> {
   return new Promise((resolve, reject) => {
     // Set interval to ping server. Store in variable so it can be cleaned before resolving.
     let checkInterval = setInterval(() => {
@@ -24,7 +24,7 @@ export function ping(address: string, endpoint: string, interval: number, checke
           if (httpResponse.statusCode >= 400) {
             reject(Error(`Server responded with status code ${httpResponse.statusCode}:\n${body}`));
           }
-          if (checker.call(thisArg, body)) {
+          if (check.call(thisArg, body, httpResponse)) {
             clearInterval(checkInterval);
             resolve();
           }
