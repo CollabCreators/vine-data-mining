d9379e8a5d65ea575605e4216c4590273f5a289f
Marko Grešak <marko@gresak.io>
2015-05-24 07:00:06 +0200

    example with orchestrate

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,27 @@ let db = Orchestrate(orchestrateKey);
 function startVine() {
   let vine = new VineApi();
 
-  vine.getUserProfile("911422956889063424")
-    .then((data: UserProfileData) => {
-    console.log(data);
+  console.time("get timeline");
+  vine.getUserTimeline("911422956889063424")
+    .then((data: Array<VineData>) => {
+    console.log("data size", data.length);
+    console.timeEnd("get timeline");
+    console.time("get mentions");
+    let mentions = VineHelper.GetUniqueMentions(data);
+    console.log("mentions size", mentions.length);
+    let mentionedUsers: Array<UserProfileData> = [];
+    Promise.all(mentions.map((m) => {
+      return vine.getUserProfile(m).then(d => mentionedUsers.push(d)).catch(error => console.log(error));
+    })).then(() => {
+      let sortedUsers = mentionedUsers.sort((a, b) => b.followerCount - a.followerCount);
+      console.log("users len:", mentionedUsers.length);
+      sortedUsers.slice(0, 5).forEach((e) => {
+        db.put("vine", e.userId, e).then((res) => console.log(res.body.results));
+      });
+      console.timeEnd("get mentions");
+    });
   })
-    .catch(error => {throw error});
+    .catch(error => { throw error });
 }
 
 startVine();
