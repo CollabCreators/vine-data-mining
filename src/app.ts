11527c569fc50989f83b00f4f0cb8de1db2420ce
Marko Grešak <marko@gresak.io>
2015-05-24 04:31:20 +0200

    add function to start a test user profile query

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,14 @@ import VineApi from "./api/VineApi";
 let username: string = process.env.VINE_USERNAME;
 let password: string = process.env.VINE_PASSWORD;
 
-let vine = new Vine(username, password);
+function startVine() {
+  let vine = new VineApi();
+
+  vine.getUserProfile(911422956889063424)
+    .then((data: UserProfileData) => {
+    console.log(data);
+  })
+    .catch(error => {throw error});
+}
+
+startVine();
