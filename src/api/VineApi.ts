0294a9b7e88314560d30e9ea8f2f165347f62c11
Marko Grešak <marko@gresak.io>
2015-05-24 04:08:57 +0200

    add reference to StoredData and import helpers

--- src/api/VineApi.ts
+++ src/api/VineApi.ts
@@ -1,9 +1,11 @@
 /// <reference path="../../typings/tsd.d.ts"/>
 /// <reference path="./api-declarations/VineAPI.d.ts"/>
+/// <reference path="./StoredData.d.ts"/>
 
 "use strict";
 import * as request from "request";
 import {Promise} from "es6-promise";
+import {UserProfileHelper, VineHelper} from "./ApiHelpers";
 
 export default class VineApi {
 
