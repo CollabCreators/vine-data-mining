dbf9d72b8aa616fdaed43543e914c007d5723599
Marko Grešak <marko@gresak.io>
2015-05-25 07:35:28 +0200

    add rejectOnError to ping function which defaults to false
    If true, returned promise will be rejected if there is an error with
    request or server responds with statusCode equal to or greater of 400.

--- src/helpers/ping.ts
+++ src/helpers/ping.ts
@@ -8,24 +8,27 @@ import {get} from "request";
  * @param   {string}                    endpoint Address endpoint to ping (starting / is not needed).
  * @param   {number}                    interval Interval in ms to ping the server.
  * @param   {(string, any?) => boolean} check    Function which returns true when result is as expected.
+ * @param   {boolean = false}      rejectOnError Should the promise be rejected if request to server fails?
  *
  * @returns {Promise<any>}                     Promise which is resolved when `check` returns true.
  */
 export default function ping(
   address: string, endpoint: string,
   interval: number, check: (body: string, res?: any) => boolean,
-  thisArg: any = this): Promise<any> {
+  thisArg: any = this, rejectOnError: boolean = false): Promise<any> {
   return new Promise((resolve, reject) => {
     // Set interval to ping server. Store in variable so it can be cleaned before resolving.
     let checkInterval = setInterval(() => {
       // Send a get request to address/endpoint and
       get({ url: `${address}/${endpoint}` },
         (err, httpResponse, body: string) => {
-          if (err) {
-            reject(err);
-          }
-          if (httpResponse.statusCode >= 400) {
-            reject(Error(`Server responded with status code ${httpResponse.statusCode}:\n${body}`));
+          if (rejectOnError) {
+            if (err) {
+              reject(err);
+            }
+            if (httpResponse.statusCode >= 400) {
+              reject(Error(`Server responded with status code ${httpResponse.statusCode}:\n${body}`));
+            }
           }
           if (check.call(thisArg, body, httpResponse)) {
             clearInterval(checkInterval);
