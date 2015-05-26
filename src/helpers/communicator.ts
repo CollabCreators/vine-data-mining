9f8e958a96d5d339d4a158a389cc856d40c3cfca
Marko Grešak <marko@gresak.io>
2015-05-26 17:00:28 +0200

    add checkErrorAndReject function
    Checks for error and calls passed reject function if there was error
    with request, if checkStatus is true.

--- src/helpers/communicator.ts
+++ src/helpers/communicator.ts
@@ -25,12 +25,7 @@ export default class Communicator {
         request.get({ url: `${address}/${endpoint}` },
           (err, httpResponse, body: string) => {
             if (rejectOnError) {
-              if (err) {
-                reject(err);
-              }
-              if (httpResponse.statusCode >= 400) {
-                reject(Error(`Server responded with status code ${httpResponse.statusCode}:\n${body}`));
-              }
+              Communicator.checkErrorAndReject(err, httpResponse, body, reject);
             }
             if (check.call(thisArg, body, httpResponse)) {
               clearInterval(checkInterval);
@@ -39,6 +34,28 @@ export default class Communicator {
           });
       }, interval);
     });
+  }
+
+  /**
+   * Check for errors in request response and call reject function if any errors found.
+   *
+   * @param {any}             err          Error from `request`.
+   * @param {any}             httpResponse Response data from `request`.
+   * @param {any}             body         Body from `request` response.
+   * @param {(any) => any}    reject       Function which is called if there was an error in response.
+   * @param {boolean = true}  checkStatus  Should status code be checked?
+   */
+  private static checkErrorAndReject(err: any, httpResponse: any, body: any, reject: (any) => any, checkStatus: boolean = true): void {
+    // Check for error in request and call reject function.
+    // Return is used so function stops, value is not expected to be used.
+    if (err) {
+      return reject(err);
+    }
+    // If checkStatus flag is true, check if response code is equal to or above 400, call reject if both is true.
+    // Return is used so function stops, value is not expected to be used.
+    if (checkStatus && httpResponse.statusCode >= 400) {
+      return reject(Error(`Server responded with status code ${httpResponse.statusCode}:\n${body}`));
+    }
   }
 
 }
