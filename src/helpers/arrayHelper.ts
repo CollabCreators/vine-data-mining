7269370651057c0458cc1319b4149b1bb53cb967
Marko Grešak <marko@gresak.io>
2015-05-28 14:01:09 +0200

    remove old mergeUnique comment

--- src/helpers/arrayHelper.ts
+++ src/helpers/arrayHelper.ts
@@ -3,14 +3,6 @@ export default class ArrayHelper {
   /**
    * Merge multiple arrays, keep unique values.
    *
-   * @param   {Array<any>} ...arrays Arrays to be merged.
-   *
-   * @returns {Array<any>}           Merged arrays.
-   */
-
-  /**
-  * Merge multiple arrays, keep unique values.
-   *
    * @param   {(x: any, i: number, arr: Array<any>) => boolean} uniqueFn  Function used to determine if value is unique,
    *                    																			pass `null` to use default to primitive value comparison.
    * @param   {Array<any>}                                      ...arrays Arrays to be merged.
