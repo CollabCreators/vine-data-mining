a943198cd0716905b7c5c9a4cdb7fdedaf735589
Marko Grešak <marko@gresak.io>
2015-05-30 11:13:17 +0200

    call nextJob from anonymous function to preserve this argument

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -96,7 +96,7 @@ export default class Worker {
     this.vineApi = new VineApi();
     // Instatiate new EventEmitter and register `job.done` event.
     this.jobEventEmitter = new EventEmitter();
-    this.jobEventEmitter.on("job.done", this.nextJob);
+    this.jobEventEmitter.on("job.done", () => this.nextJob());
     this.workerProfiler = new WorkerProfiler(this.jobEventEmitter, Worker.TIME_THRESHOLD);
     this.jobSize = 1;
     this.resetCounters();
