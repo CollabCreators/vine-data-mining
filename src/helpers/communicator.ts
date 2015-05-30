4cfd47fa8c150dcd10d38e1574fd9faf70dafaac
Marko Grešak <marko@gresak.io>
2015-05-30 09:41:33 +0200

    remove checking if address and registered address matches

--- src/helpers/communicator.ts
+++ src/helpers/communicator.ts
@@ -73,22 +73,8 @@ export default class Communicator {
         (err, httpResponse, body: string) => {
           // Check for errors, calls reject if there are any.
           Communicator.checkErrorAndReject(err, httpResponse, body, reject);
-          let address = "";
-          // Try to parse address or reject the value.
-          try {
-            address = JSON.parse(body).address;
-          }
-          catch (e) {
-            reject(e);
-          }
-          // If address in response (new address) doesn't match the address which was sent, try to register it again.
-          if (address !== ipAddress) {
-            Communicator.registerAddress(server, endpoint, ipAddress).then(resolve);
-          }
-          else {
-            // Register was successful, resolve returned promise.
-            resolve(body);
-          }
+          // Register was successful, resolve returned promise.
+          resolve(body);
         });
     });
   }
