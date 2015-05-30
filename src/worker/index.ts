b872fb0b98312b16b9a30c51e617d3cbf6cefdff
Marko Grešak <marko@gresak.io>
2015-05-30 04:35:45 +0200

    instantiate new VineApi

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,15 @@
+import VineApi from "../api/VineApi";
 export default class Worker {
 
+  /**
+   * Instance of Vine API communicator.
+   *
+   * @type {VineApi}
+   */
+  private vineApi: VineApi;
+
   constructor(masterPort: number) {
+    this.vineApi = new VineApi();
   }
 
 }
