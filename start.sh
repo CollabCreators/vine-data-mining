3e335a3bdd0fb01849c6efb56866c53b464a5397
Marko Grešak <marko@gresak.io>
2015-05-30 09:59:22 +0200

    disable reject unauthorized tls in start.sh
    This caused errors when connecting to router.

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 command -v forever >/dev/null 2>&1 || { echo >&2 "Forever not installed, use 'npm install -g forever'. Aborting."; exit 1; }
+export NODE_TLS_REJECT_UNAUTHORIZED=0
 runArg=""
 if [[ "$1" == "router" ]] || [[ "$1" == "r" ]]; then runArg="router"
 elif [[ "$1" == "master" ]] || [[ "$1" == "m" ]]; then runArg="master"
