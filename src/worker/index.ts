1337da28a1534bbd229a642da179e05bb09bfffa
Marko Grešak <marko@gresak.io>
2015-05-30 05:46:38 +0200

    use EventEmitter to call nextJob function on job.done event

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,6 @@
 import VineApi from "../api/VineApi";
 import Communicator from "../helpers/communicator";
+import {EventEmitter} from "events";
 
 export default class Worker {
 
@@ -18,12 +19,24 @@ export default class Worker {
   private masterAddress: string;
 
   /**
+   * Event system for managing when job starts / ends.
+   *
+   * @type {EventEmitter}
+   */
+  private jobEventEmitter: EventEmitter;
+
+  /**
    * Start a new worker. Will need router and master running to start.
    *
    * @param   {number} masterPort Port where master node is listening.
    */
   constructor(public masterPort: number) {
     this.vineApi = new VineApi();
+    this.jobEventEmitter = new EventEmitter();
+    this.jobEventEmitter.on("job.done", this.nextJob);
+    this.nextJob();
+  }
+  private nextJob(): void {
     Communicator.getAddress().then((address: string) => {
       this.masterAddress = `http://${address}:${masterPort}/master`;
       this.run();
