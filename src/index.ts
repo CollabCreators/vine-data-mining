b069b94210f1a31c112e0588301af9592cd1b492
Marko Grešak <marko@gresak.io>
2015-06-01 01:54:33 +0200

    kill any processes on port 9631 before starting new Router or Master

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 import Router from "./router/index";
 import Master from "./master/index";
 import Worker from "./worker/index";
+import {exec} from "child_process";
 
 // Set environemnt variable NODE_TLS_REJECT_UNAUTHORIZED (to allow connection to gresak.io) router.
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
@@ -24,14 +25,26 @@ const PORT = 9631;
 const HELP_ARGS = ["-h", "--help", "help"];
 
 /**
+ * Kill applications on `PORT`.
+ * (NOTE: Works on Linux / OS X only)
+ *
+ * @param   {Function} cb Function to call when exec is done.
+ */
+function killPort(cb) {
+  exec(`lsof -i | grep :${PORT} | awk '{print $2}' | xargs kill`, cb);
+}
+
+/**
  * Key(arg) - value(constructor) map for argument and it's corresponding constructor.
  *
  * @type {Object}
  */
-const CONSTRUCTORS = {
-  "router": Router,
-  "master": Master,
-  "worker": Worker
+const initFunctions = {
+  // Router and master require `PORT` to be free, but worker does not,
+  // therefore killing other applications is not necessary.
+  "router": () => killPort(() => new Router(PORT)),
+  "master": () => killPort(() => new Master(PORT)),
+  "worker": () => new Worker(PORT)
 }
 
 // Get argument or use default value empty string.
@@ -51,14 +64,14 @@ if (HELP_ARGS.indexOf(runArg) >= 0) {
     \trouter - run router server on port ${PORT}
     \tworker - run a worker
     `);
-    process.exit(0);
+  process.exit(0);
 }
 
 // If run argument isn't a help message and it doesn't match any of `CONSTRUCTORS`, print error message and exit.
-if (Object.keys(CONSTRUCTORS).indexOf(runArg) === -1) {
+if (Object.keys(initFunctions).indexOf(runArg) === -1) {
   console.error("Unsupported run argument!");
   process.exit(1);
 }
 
-// Instantiate a new service based on runArgs and pass it the `PORT`.
-new CONSTRUCTORS[runArg](PORT);
+// Call a constructor based on runArgs and pass it the `PORT`.
+initFunctions[runArg]();
