22e23fdd7b47518a0b3e486e34f9bfc869a90711
Marko Grešak <marko@gresak.io>
2015-06-01 01:00:45 +0200

    add description to isJobType function

--- src/master/JobTypes.ts
+++ src/master/JobTypes.ts
@@ -3,6 +3,13 @@ export default class JobTypes {
   public static Vine: JobType = 1;
   public static Unknown: JobType = -1;
 
+  /**
+   * Check if given type is a jobType.
+   *
+   * @param   {any}     type Type to check.
+   *
+   * @returns {boolean}
+   */
   public static isJobType(type: any): boolean {
     let x = parseInt(type);
     return x === JobTypes.User || x === JobTypes.Vine;
