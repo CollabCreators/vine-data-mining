0c1764bb3ecb0955800433e2e44fa2eea3c97dc8
Marko Grešak <marko@gresak.io>
2015-06-01 04:33:45 +0200

    add optional argument -p [path] to start script

--- start.sh
+++ start.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
+
+foreverPath="./"
+OPTIND=1
+while getopts "p:" opt; do
+  case "$opt" in
+    p)  foreverPath=$OPTARG ;;
+  esac
+done
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
 runArg="${1:-worker}"
 if [[ "$runArg" == "router" ]] || [[ "$runArg" == "r" ]]; then runArg="router"
 elif [[ "$runArg" == "master" ]] || [[ "$runArg" == "m" ]]; then runArg="master"
 elif [[ "$runArg" == "worker" ]] || [[ "$runArg" == "w" ]]; then runArg="worker"; fi
 
 if type forever >/dev/null; then
-  forever start -c "npm start $runArg" ./
+  forever start -c "npm start $runArg" $foreverPath
 elif type /usr/local/bin/forever >/dev/null; then
-  /usr/local/bin/forever start -c "npm start $runArg" ./
+  /usr/local/bin/forever start -c "npm start $runArg" $foreverPath
 else
   echo >&2 "Forever not installed, use 'npm install -g forever'. Aborting."
   exit 1
