cb2a6d159f67c905f647c65747720c6cb8c8b6ff
Marko Grešak <marko@gresak.io>
2015-06-01 03:39:04 +0200

    remove exporting NODE_TLS_REJECT_UNAUTHORIZED in start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 command -v forever >/dev/null 2>&1 || { echo >&2 "Forever not installed, use 'npm install -g forever'. Aborting."; exit 1; }
-export NODE_TLS_REJECT_UNAUTHORIZED=0
 runArg="${1:-worker}"
 if [[ "$runArg" == "router" ]] || [[ "$runArg" == "r" ]]; then runArg="router"
 elif [[ "$runArg" == "master" ]] || [[ "$runArg" == "m" ]]; then runArg="master"
