d0aeca4ce197160e9d33fe605faa919327a29d66
Marko Grešak <marko@gresak.io>
2015-06-01 02:23:54 +0200

    make worker default runArg in start script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 command -v forever >/dev/null 2>&1 || { echo >&2 "Forever not installed, use 'npm install -g forever'. Aborting."; exit 1; }
 export NODE_TLS_REJECT_UNAUTHORIZED=0
-runArg=""
-if [[ "$1" == "router" ]] || [[ "$1" == "r" ]]; then runArg="router"
-elif [[ "$1" == "master" ]] || [[ "$1" == "m" ]]; then runArg="master"
-elif [[ "$1" == "worker" ]] || [[ "$1" == "w" ]]; then runArg="worker"; fi
+runArg="${1:-worker}"
+if [[ "$runArg" == "router" ]] || [[ "$runArg" == "r" ]]; then runArg="router"
+elif [[ "$runArg" == "master" ]] || [[ "$runArg" == "m" ]]; then runArg="master"
+elif [[ "$runArg" == "worker" ]] || [[ "$runArg" == "w" ]]; then runArg="worker"; fi
 forever start -c "npm start $runArg" ./
