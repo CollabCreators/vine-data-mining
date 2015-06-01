9abe75b8732e2e7b5bc70f39802c264c94c7b36d
Marko Grešak <marko@gresak.io>
2015-06-01 03:40:15 +0200

    check for both forever and /usr/local/bin/forever in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-command -v forever >/dev/null 2>&1 || { echo >&2 "Forever not installed, use 'npm install -g forever'. Aborting."; exit 1; }
 runArg="${1:-worker}"
 if [[ "$runArg" == "router" ]] || [[ "$runArg" == "r" ]]; then runArg="router"
 elif [[ "$runArg" == "master" ]] || [[ "$runArg" == "m" ]]; then runArg="master"
 elif [[ "$runArg" == "worker" ]] || [[ "$runArg" == "w" ]]; then runArg="worker"; fi
-forever start -c "npm start $runArg" ./
+
+if type forever >/dev/null; then
+  forever start -c "npm start $runArg" ./
+elif type /usr/local/bin/forever >/dev/null; then
+  /usr/local/bin/forever start -c "npm start $runArg" ./
+else
+  echo >&2 "Forever not installed, use 'npm install -g forever'. Aborting."
+  exit 1
+fi
