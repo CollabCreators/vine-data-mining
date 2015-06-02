256f01d6537c06a50b9caa492f2bb8ddaac878a9
Marko Grešak <marko@gresak.io>
2015-06-02 09:50:46 +0200

    use storage/ as LocalStorage base path, add it to gitignore

--- src/simple-worker/local-storage.ts
+++ src/simple-worker/local-storage.ts
@@ -1,4 +1,12 @@
+import * as path from "path";
 export default class LocalStorage {
+
+  /**
+   * Path to storage directory.
+   *
+   * @type {string}
+   */
+   private static BASE_PATH = path.resolve("./storage");
 
   constructor() {
   }
