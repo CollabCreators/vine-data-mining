4777b0c3622aa6f39188a1241acd48d6a75c78e7
Marko Grešak <marko@gresak.io>
2015-06-02 17:08:12 +0200

    add constants for file paths

--- src/simple-worker/local-storage.ts
+++ src/simple-worker/local-storage.ts
@@ -8,7 +8,26 @@ export default class LocalStorage {
    *
    * @type {string}
    */
-   private static BASE_PATH = path.resolve("./storage");
+  private static BASE_PATH = "./storage/";
+
+  /**
+   * File name of data storage file.
+   *
+   * @type {string}
+   */
+  private static DATA_FILENAME = path.resolve(LocalStorage.BASE_PATH, "data.json");
+
+  /**
+   * File name of jobs storage file.
+   *
+   * @type {string}
+   */
+  private static JOBS_FILENAME = path.resolve(LocalStorage.BASE_PATH, "jobs.json");
+
+  /**
+   * File name of size storage file.
+   */
+  private static SIZE_FILENAME = path.resolve(LocalStorage.BASE_PATH, "size.json");
 
   constructor() {
     mkdirp.sync(LocalStorage.BASE_PATH);
