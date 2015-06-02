78f45322f5c3d5f2fb9e7b7ac92cc535551552a2
Marko Grešak <marko@gresak.io>
2015-06-02 17:12:59 +0200

    implement getFirstJobs
    Returns first n jobs from file. Using exec head and tail
    because it was easier than figuring out how streams work.
    Somewhat error prof by resolving to empty array on error
    so value, although empty, can still be used.

--- src/simple-worker/local-storage.ts
+++ src/simple-worker/local-storage.ts
@@ -1,6 +1,8 @@
 import * as path from "path";
 import * as fs from "fs";
 import * as mkdirp from "mkdirp";
+import {exec} from "child_process";
+import Job from "../master/job";
 
 export default class LocalStorage {
 
@@ -68,6 +70,44 @@ export default class LocalStorage {
     return LocalStorage.appendFile(LocalStorage.SIZE_FILENAME, { timestamp: Date.now(), size: size });
   }
 
+  /**
+   * Get first `n` jobs from file and remove these jobs from file.
+   *
+   * @param   {number}  n             Number of jobs to get.
+   *
+   * @returns {Promise<Array<Job>>}   Promise resolving with array of `n` jobs.
+   */
+  public getFirstJobs(n: number): Promise<Array<Job>> {
+    return new Promise((resolve, reject) => {
+      // Get first `n` jobs from file.
+      exec(`head -${n} ${LocalStorage.JOBS_FILENAME}`, (err, stdout: string) => {
+        // Resolve promise with empty array if there was an error.
+        if (err) {
+          resolve([]);
+        }
+        // Make a promise resolving with all job uids mapped to jobs.
+        let jobsPromise: Promise<Array<Job>> = new Promise((resolve) => {
+          try {
+            let jobsFromUid = stdout.split("\n").map((uid) => Job.JobFromUid(uid)).filter((j) => j !== null);
+            resolve(jobsFromUid);
+          }
+          catch (e) {
+            // If there was an error, resolve with empty array.
+            resolve([]);
+          }
+        });
+        // Remove first `n` fobs from file.
+        exec(`tail -n+${n + 1} ${LocalStorage.JOBS_FILENAME} > ${LocalStorage.JOBS_FILENAME}`, (err) => {
+          // Resolve promise with empty array if there was an error.
+          if (err) {
+            resolve([]);
+          }
+          // Resolve returned promise when jobsPromise resolves (resolved with jobs data).
+          jobsPromise.then(resolve);
+        });
+      });
+    });
+  }
    * Promise wrapper for `fs.appendFile`.
    *
    * @param   {string}       fileName Name of file to write (append) to.
